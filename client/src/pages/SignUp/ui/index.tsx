4790c6e36330086365ed2865bb3e33a1dfbb9b36
tanziqly <lhonston@gmail.com>
2025-02-19 22:44:20 +0300

    Sign up

--- client/src/pages/SignUp/ui/index.tsx
+++ client/src/pages/SignUp/ui/index.tsx
@@ -1,5 +1,12 @@
+import { RegForm } from "@features/RegForm";
 import { FC } from "react";
 
 export const SignUp: FC = () => {
-  return <div>SignUp page</div>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <RegForm />
+      </div>
+    </div>
+  );
 };
