2af2b730d7f9885273f8a72e18d22cf2bbac6aeb
tanziqly <lhonston@gmail.com>
2025-02-21 00:27:05 +0300

    Product ui

--- client/src/entities/Product/ui/index.tsx
+++ client/src/entities/Product/ui/index.tsx
@@ -3,7 +3,7 @@ import Product from "./Product";
 export const ProductWrapper = () => {
   return (
     <section className="flex-center">
-      <div className="width grid gap-4 grid-cols-4">
+      <div className="width grid gap-4 grid-cols-1 sm:grid-cols-2 justify-items-center lg:grid-cols-4">
         {Array.from({ length: 5 }).map((_, index) => (
           <Product />
         ))}
