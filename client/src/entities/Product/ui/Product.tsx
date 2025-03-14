36f612745d24dc53e17860e622f5e2b5943500dc
tanziqly <lhonston@gmail.com>
2025-03-14 14:53:42 +0300

    product hover

--- client/src/entities/Product/ui/Product.tsx
+++ client/src/entities/Product/ui/Product.tsx
@@ -3,14 +3,16 @@ import image from "./product.jpeg";
 
 const Product = () => {
   return (
-    <div className="flex flex-col hover:bg-neutral-100 dark:hover:bg-neutral-800 rounded-sm max-w-[240px] w-full">
-      <Link to="/profile" className="flex flex-col">
-        <img src={image} className="w-full h-56 rounded-t-sm" alt="Product" />
-        <div className="flex flex-col gap-1 p-2">
-          <h3 className="text-xl font-bold">Product1</h3>
-          <span className="text-neutral-500">
-            Lorem ipsum asdasddsdsad dolor sit amet.
-          </span>
+    <div className="flex transition duration-300 ease-in-out hover:scale-105 flex-col hover:bg-neutral-100 dark:hover:bg-neutral-800 rounded-sm max-w-[240px] w-full">
+      <Link to="/product" className="block">
+        <div className="flex flex-col">
+          <img src={image} className="w-full h-56 rounded-t-sm" alt="Product" />
+          <div className="flex flex-col gap-1 p-2">
+            <h3 className="text-xl font-bold">Product1</h3>
+            <span className="text-neutral-500">
+              Lorem ipsum asdasddsdsad dolor sit amet.
+            </span>
+          </div>
         </div>
       </Link>
       <div className="flex justify-between mt-2 p-2">
